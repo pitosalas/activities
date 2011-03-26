88d963dbc14cbfc46dcb90fff6b13015e6c9b165
Pito Salas <rps@salas.com>
2011-03-26 14:15:31 -0400

    second commit

---
+++ src/edu/brandeis/cosi/Apple.java
@@ -0,0 +1,13 @@
+package edu.brandeis.cosi;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Apple extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
